50b140afdb0af0af25d127f68581a206f33eb69a
Davi Aquila <daviaquila162@gmail.com>
2020-12-13 19:02:44 -0300

    First Commit

---
+++ app/src/main/java/com/debcomp/aql/sp42/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.debcomp.aql.sp42
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
